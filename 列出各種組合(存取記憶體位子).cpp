dd1e60a695c5e6c854865ac4f0013e8a12b745bf
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2018-07-05 23:47:21 +0800

    i don't know= =

--- 列出各種組合(存取記憶體位子).cpp
+++ 列出各種組合(存取記憶體位子).cpp
@@ -11,26 +11,31 @@ char buffer;
 int num1=1000;
 
 int getstringlength(char *s);
-void down(int num,char *s);
-void down(int *num);
+void down(int num,int *s);
+void down_(int *loadnum);
+void down_d(int num,int *s);
 void create_data(char *s,char chr);
 
 int main()
 {
-	fopen("test.txt","w");
+	fclose(fopen("test.txt","w"));
 	printf("½Ð¿é¤J¼Æ¦r");
 	scanf("%s",st);
 	num1 = getstringlength(st);
 	for(int loopnum1=num1-1;loopnum1 >= 0;loopnum1--){
+		int b[num1];
+		b[num1-1]=loopnum1;
+		down(num1-2,b);
+		
 		int c[num1+1];
 		c[num1]=loopnum1;
 		c[0] = num1-2;
-		//down(num1-2,c);
-		down(c);
+		_beginthread(down_, (num1-1)-loopnum1,c);
+		//down_(c);
 	}	
 } 
 
-void down(int num,char *s){
+void down(int num,int *s){
 	if(num != -1){
 		for(int loopnum2=num1-1;loopnum2 >= 0;loopnum2--){
 			for(int loopnum4=num+1;loopnum4 <= num1-1;loopnum4++){
@@ -52,8 +57,53 @@ void down(int num,char *s){
 	}
 }
 
-void down(int *num){
-	down(num,num+1);
+
+void down_d(int num,int *s){
+	if(num != -1){
+		for(int loopnum2=num1-1;loopnum2 >= 0;loopnum2--){
+			for(int loopnum4=num+1;loopnum4 <= num1-1;loopnum4++){
+				if(*(s+loopnum4)!=loopnum2){
+					if(loopnum4 == num1-1){ 
+						*(s+num)=loopnum2;
+						down_d(num-1,s);
+					} 
+				}else{
+					break;
+				}
+			}	
+		}
+	}else{
+		char str[num1];
+		str[0] = st[*(s+num1-1)];
+		str[1] = '\0';
+		char file[getstringlength(str)+10];
+		file[0] = '\0';
+		strcat(file, "test_");
+		strcat(file, str);
+		strcat(file, ".txt");
+		for(int loopnum3=num1-1;loopnum3 >= 0;loopnum3--){
+			
+			create_data(file,st[*(s+loopnum3)]);
+		}
+		create_data(file,'\n');
+	}
+}
+
+void down_(int *loadnum){
+	char str[num1];
+	str[0] = st[*(loadnum+1+num1-1)];
+	str[1] = '\0';
+	char file[getstringlength(str)+10];
+	file[0] = '\0';
+	strcat(file, "test_");
+	strcat(file, str);
+	strcat(file, ".txt");
+	fclose(fopen(file,"w"));
+	
+	int num = *loadnum;
+	int *s;
+	s=loadnum+1;
+	down_d(num,s);
 }
 
 int getstringlength(char *s){
