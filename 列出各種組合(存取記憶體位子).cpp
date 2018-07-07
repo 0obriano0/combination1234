5b2edeb4611baaf38b8c98b53cb56e0766e4cfaa
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2018-07-07 20:21:25 +0800

    fix

--- 列出各種組合(存取記憶體位子).cpp
+++ 列出各種組合(存取記憶體位子).cpp
@@ -7,65 +7,41 @@
 #include <windows.h>
 
 char st[1000];
-char buffer;
-int num1=1000;
 
 int getstringlength(char *s);
-void down(int num,int *s);
 void down_(int *loadnum);
-void down_d(int num,int *s);
+void down_d(int num,int data2,int *s,int *numbox);
 void create_data(char *s,char chr);
 
 int main()
 {
+	int num1=1000;
 	fclose(fopen("test.txt","w"));
 	printf("½Ð¿é¤J¼Æ¦r");
 	scanf("%s",st);
 	num1 = getstringlength(st);
 	for(int loopnum1=num1-1;loopnum1 >= 0;loopnum1--){
-		int b[num1];
-		b[num1-1]=loopnum1;
-		down(num1-2,b);
-		
-		int c[num1+1];
-		c[num1]=loopnum1;
+		int c[(num1+1)*2+1];
 		c[0] = num1-2;
-		//_beginthread(down_, (num1-1)-loopnum1,c);
+		c[1] = num1;
+		c[2] = num1+3;
+		c[(num1+1)*2]=loopnum1;
+		for(int loopnum2 = 0;loopnum2 < num1;loopnum2++){
+			c[loopnum2+3] = st[loopnum2];
+		}
+		//_beginthread(down_,0,c);
 		down_(c);
 	}	
-} 
-
-void down(int num,int *s){
-	if(num != -1){
-		for(int loopnum2=num1-1;loopnum2 >= 0;loopnum2--){
-			for(int loopnum4=num+1;loopnum4 <= num1-1;loopnum4++){
-				if(*(s+loopnum4)!=loopnum2){
-					if(loopnum4 == num1-1){ 
-						*(s+num)=loopnum2;
-						down(num-1,s);
-					} 
-				}else{
-					break;
-				}
-			}	
-		}
-	}else{
-		for(int loopnum3=num1-1;loopnum3 >= 0;loopnum3--){
-			create_data("test.txt",st[*(s+loopnum3)]);
-		}
-		create_data("test.txt",'\n');
-	}
 }
 
-
-void down_d(int num,int *s){
+void down_d(int num,int data2,int *s,int *numbox){
 	if(num != -1){
-		for(int loopnum2=num1-1;loopnum2 >= 0;loopnum2--){
-			for(int loopnum4=num+1;loopnum4 <= num1-1;loopnum4++){
+		for(int loopnum2=data2-1;loopnum2 >= 0;loopnum2--){
+			for(int loopnum4=num+1;loopnum4 <= data2-1;loopnum4++){
 				if(*(s+loopnum4)!=loopnum2){
-					if(loopnum4 == num1-1){ 
+					if(loopnum4 == data2-1){ 
 						*(s+num)=loopnum2;
-						down_d(num-1,s);
+						down_d(num-1,data2,s,numbox);
 					} 
 				}else{
 					break;
@@ -73,25 +49,32 @@ void down_d(int num,int *s){
 			}	
 		}
 	}else{
-		char str[num1];
-		str[0] = st[*(s+num1-1)];
+		char str[data2];
+		str[0] = *(numbox+*(s+data2-1));
 		str[1] = '\0';
 		char file[getstringlength(str)+10];
 		file[0] = '\0';
 		strcat(file, "test_");
 		strcat(file, str);
 		strcat(file, ".txt");
-		for(int loopnum3=num1-1;loopnum3 >= 0;loopnum3--){
+		for(int loopnum3=data2-1;loopnum3 >= 0;loopnum3--){
 			
-			create_data(file,st[*(s+loopnum3)]);
+			create_data(file,*(numbox+*(s+loopnum3)));
 		}
 		create_data(file,'\n');
 	}
 }
 
 void down_(int *loadnum){
-	char str[num1];
-	str[0] = st[*(loadnum+1+num1-1)];
+	int data1 = *loadnum;
+	int data2 = *loadnum+1;
+	int data3 = *loadnum+2;
+	int *s1,*s2;
+	s1=loadnum+3;
+	s2=loadnum+data3;
+	
+	char str[data2];
+	str[0] = st[*(s2+1+data2-1)];
 	str[1] = '\0';
 	char file[getstringlength(str)+10];
 	file[0] = '\0';
@@ -100,10 +83,7 @@ void down_(int *loadnum){
 	strcat(file, ".txt");
 	fclose(fopen(file,"w"));
 	
-	int num = *loadnum;
-	int *s;
-	s=loadnum+1;
-	down_d(num,s);
+	down_d(data1,data2,s2,s1);
 }
 
 int getstringlength(char *s){
