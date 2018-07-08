96dcd8b8a3c1f8fcf19053a6f9bbde8ec577be8e
0obriano0 <brian041301@gmail.com>
2018-07-08 21:27:19 +0800

    all done

--- 列出各種組合(陣列處理_多執行續).cpp
+++ 列出各種組合(陣列處理_多執行續).cpp
@@ -8,45 +8,66 @@
 
 int getstringlength(char *s);
 void down_(int *loadnum);
-void down_d(int num,int data2,char *s,int *numbox);
+void down_d(int num,int data2,char *s,int *numbox,int *data_run);
 int create_data(char *s,char chr);
+void int2str(int i, char *s);
 
 int main()
 {
-	char st[1000];
+	char st[100];
 	int num1=1000;
-	fclose(fopen("test.txt","w"));
-	printf("請輸入數字");
+	printf("請輸入資料(最多只能輸入100個值):");
 	scanf("%s",st);
 	num1 = getstringlength(st);
 	int *c;
+	int *ptr[100];
 	for(int loopnum1=num1-1;loopnum1 >= 0;loopnum1--){
 		do{
-			c = (int)malloc(sizeof(int)*((num1+1)*2+1));
+			c = (int)malloc(sizeof(int)*((num1+1)*2+2));
 		}while(c==NULL);
 		*c = num1-2;
 		*(c+1) = num1;
 		*(c+2) = num1+3;
 		*(c+((num1+1)*2))=loopnum1;
+		*(c+((num1+1)*2+1))=0;
+		printf("c[%d] = %p\n",loopnum1,c+((num1+1)*2+1));
+		ptr[loopnum1] = c; 
 		for(int loopnum2 = 0;loopnum2 < num1;loopnum2++){
 			*(c+loopnum2+3) = st[loopnum2]-'0';
 		}
 		_beginthread(down_,0,c);
 		//down_(c);
 	}
-	while(1){
-		
+	bool thread_all_finish = false;
+	int thread_data_length = 1;
+	for(int loopnum2=num1-1;loopnum2 >= 1;loopnum2--){
+		thread_data_length*=loopnum2;
 	}
+	while(!thread_all_finish){
+		//printf("thread_data_length = %d \n",thread_data_length);
+		for(int loopnum3=num1-1;loopnum3 >= 0;loopnum3--){
+			//printf("ptr[%d] = %d 記憶體 = %p\n",loopnum1,*(ptr[loopnum1]+((num1+1)*2)+1),ptr[loopnum1]+((num1+1)*2)+1);
+			if(*(ptr[loopnum3]+((num1+1)*2)+1) == thread_data_length){
+				thread_all_finish = true;
+			}else{
+				thread_all_finish = false;
+				break;
+			}
+		}
+	}
+	for(int loopnum4=num1-1;loopnum4 >= 0;loopnum4--)
+		free(ptr[loopnum4]);
+	system("pause");
 }
 
-void down_d(int num,int data2,char *st,int *s){
+void down_d(int num,int data2,char *st,int *s,int *data_run){
 	if(num != -1){
 		for(int loopnum2=data2-1;loopnum2 >= 0;loopnum2--){
 			for(int loopnum4=num+1;loopnum4 <= data2-1;loopnum4++){
 				if(*(s+loopnum4)!=loopnum2){
 					if(loopnum4 == data2-1){ 
 						*(s+num)=loopnum2;
-						down_d(num-1,data2,st,s);
+						down_d(num-1,data2,st,s,data_run);
 					} 
 				}else{
 					break;
@@ -54,20 +75,19 @@ void down_d(int num,int data2,char *st,int *s){
 			}	
 		}
 	}else{
-		
-		char str[data2];
-		str[0] = st[*(s+data2-1)];
-		str[1] = '\0';
-		char file[getstringlength(str)+10];
+		char str[100];
+    	int2str(*(s+data2-1)+1, str);
+		char file[getstringlength(str)+16];
 		file[0] = '\0';
-		strcat(file, "test_");
+		strcat(file, "data//test_");
 		strcat(file, str);
 		strcat(file, ".txt");
-		
 		for(int loopnum3=data2-1;loopnum3 >= 0;loopnum3--){
 			create_data(file,st[*(s+loopnum3)]);
 		}
 		create_data(file,'\n');
+		*data_run += 1;
+		printf("data_run[%d] = %d 記憶體 = %p\n",*(s+data2-1)+1,*data_run,data_run);
 	}
 }
 
@@ -75,24 +95,23 @@ void down_(int *loadnum){
 	int data1 = *loadnum;
 	int data2 = *(loadnum+1);
 	int data3 = *(loadnum+2);
+	int *data4 = (loadnum+((data2+1)*2+1));
 	int *s1;
 	s1=loadnum+data3;
 	char st[data2];
 	for(int loopnum1 = 0;loopnum1 < data2;loopnum1++){
 		st[loopnum1] = *(loadnum+3+loopnum1)+'0';
 	}
-	
-	char str[data2];
-	str[0] = st[*(s1+data2-1)];
-	str[1] = '\0';
-	char file[getstringlength(str)+10];
+	char str[100];
+    int2str(*(s1+data2-1)+1, str);
+	char file[getstringlength(str)+16];
 	file[0] = '\0';
-	strcat(file, "test_");
+	strcat(file, "data//test_");
 	strcat(file, str);
 	strcat(file, ".txt");
 	fclose(fopen(file,"w"));
 	
-	down_d(data1,data2,st,s1);
+	down_d(data1,data2,st,s1,data4);
 }
 
 int getstringlength(char *s){
@@ -115,4 +134,8 @@ int create_data(char *s,char chr){
 		fprintf(pf,"%c",chr);
 		fclose(pf);
 	}
+}
+
+void int2str(int i, char *s){
+	sprintf(s,"%d",i);
 }
